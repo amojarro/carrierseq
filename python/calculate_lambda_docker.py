6a936388e9b96942df41e446da05649adc6b21e4
Angel Mojarro <angel.mojarro@gmail.com>
2017-06-29 14:26:01 -0400

    Add files via upload

---
+++ python/calculate_lambda_docker.py
@@ -0,0 +1,18 @@
+import math
+
+print 'Lambda = Unkown Reads / Used Channels'
+
+reads_txt = open('/carrierseq/05_target_reads/carrierseq_out.txt', 'r')
+reads = reads_txt.read()
+print 'Unknown Reads (05_target_reads/carrierseq_out.txt)'
+print reads
+
+channels_txt = open('/carrierseq/06_poisson_calculation/channels_in_use.txt', 'r')
+channels = channels_txt.read()
+print 'Used Channels (channels_in_use.txt)'
+print channels
+
+lambda_value = (float(reads) / float(channels))
+print 'Lambda:'
+print lambda_value
+
---
+++ python/xcrit_docker.py
@@ -0,0 +1,19 @@
+from scipy.stats import poisson
+
+# print 'Calculate  =  Reads/Channel Threshold for Potential Noise Given Lambda'
+
+lambda_value_txt = open('/carrierseq/06_poisson_calculation/lambda_value.txt', 'r')
+lambda_value = lambda_value_txt.read().splitlines()[8]
+
+print 'Lambda Value (06_poisson_calculation/lambda_value.txt)'
+print lambda_value
+
+p = 0.05 # User Defined
+print 'P Value'
+print p
+
+x_crit = poisson.ppf(1-p,float(lambda_value))
+ 
+print 'Critical Read/Channel Threshold'
+print x_crit
+
