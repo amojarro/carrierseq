b9f3602dd3bea240a13ab3df6ae842e46f814f0b
Angel Mojarro <angel.mojarro@gmail.com>
2017-06-29 14:21:15 -0400

    Add files via upload

--- python/calculate_lambda.py
+++ python/calculate_lambda.py
@@ -2,12 +2,12 @@ import math
 
 print 'Lambda = Unkown Reads / Used Channels'
 
-reads_txt = open('/DataFolder/05_target_reads/carrierseq_out.txt', 'r')
+reads_txt = open('/<your/working/directory>/05_target_reads/carrierseq_out.txt', 'r')
 reads = reads_txt.read()
 print 'Unknown Reads (05_target_reads/carrierseq_out.txt)'
 print reads
 
-channels_txt = open('/DataFolder/06_poisson_calculation/channels_in_use.txt', 'r')
+channels_txt = open('/<your/working/directory>/06_poisson_calculation/channels_in_use.txt', 'r')
 channels = channels_txt.read()
 print 'Used Channels (channels_in_use.txt)'
 print channels
--- python/xcrit.py
+++ python/xcrit.py
@@ -2,8 +2,8 @@ from scipy.stats import poisson
 
 # print 'Calculate  =  Reads/Channel Threshold for Potential Noise Given Lambda'
 
-lambda_value_txt = open('/DataFolder/06_poisson_calculation/lambda_value.txt', 'r')
-lambda_value = lambda_value_txt.read().splitlines()[6]
+lambda_value_txt = open('/<your/working/directory>/06_poisson_calculation/lambda_value.txt', 'r')
+lambda_value = lambda_value_txt.read().splitlines()[8]
 
 print 'Lambda Value (06_poisson_calculation/lambda_value.txt)'
 print lambda_value
@@ -14,6 +14,6 @@ print p
 
 x_crit = poisson.ppf(1-p,float(lambda_value))
  
-print 'Critical Read/Channel Threshhold'
+print 'Critical Read/Channel Threshold'
 print x_crit
 
