5c5ed0f148d88698ee0368be57c94aa9a770543d
Angel Mojarro <angel.mojarro@gmail.com>
2017-08-01 11:16:18 -0400

    Update xcrit.py

--- python/xcrit.py
+++ python/xcrit.py
@@ -4,7 +4,7 @@ import sys
 lambda_value_txt = open(sys.argv[1], 'r')
 lambda_value = lambda_value_txt.read().splitlines()[8]
 
-print 'Lambda Value (06_poisson_calculation/lambda_value.txt)'
+print 'Lambda Value'
 print lambda_value
 
 p = float(sys.argv[2]) # User Defined
