c4293eb55b0033f1c3ed4bfd07564023fbfa65e9
Angel Mojarro <angel.mojarro@gmail.com>
2017-08-01 10:23:14 -0400

    Update xcrit.py

--- python/xcrit.py
+++ python/xcrit.py
@@ -1,14 +1,15 @@
 from scipy.stats import poisson
+import sys
 
 # print 'Calculate  =  Reads/Channel Threshold for Potential Noise Given Lambda'
 
-lambda_value_txt = open('/<your/working/directory>/06_poisson_calculation/lambda_value.txt', 'r')
+lambda_value_txt = open(sys.argv[1], 'r')
 lambda_value = lambda_value_txt.read().splitlines()[8]
 
 print 'Lambda Value (06_poisson_calculation/lambda_value.txt)'
 print lambda_value
 
-p = 0.05 # User Defined
+p = float(sys.argv[2]) # User Defined
 print 'P Value'
 print p
 
@@ -16,4 +17,3 @@ x_crit = poisson.ppf(1-p,float(lambda_value))
  
 print 'Critical Read/Channel Threshold'
 print x_crit
-
